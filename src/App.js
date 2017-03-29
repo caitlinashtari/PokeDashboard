db636ce755430c0d4352681150f54a835ed0d399
caitlin ashtari <caitlin.a@lumenlearning.com>
2017-03-29 16:56:26 -0700

    setup initial file structure with create react app; make api call to pokeapi to return pokemon in console.

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'whatwg-fetch';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.loadPokemon = this.loadPokemon.bind(this);
+  }
+
+  loadPokemon(url) {
+    fetch(url)
+    .then(response => {
+      return response.json();
+    }).then(json => {
+      console.log(json)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+// call method before page loads
+  componentWillMount() {
+    this.loadPokemon(`${this.props.baseUrl}/pokemon`);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
