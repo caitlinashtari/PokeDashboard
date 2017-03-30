61e7ddf9a34fd711d84aaab9329a32c58a86ef01
caitlin ashtari <caitlin.a@lumenlearning.com>
2017-03-30 09:55:26 -0700

    add react bootstrap and output poke names to cols by creating a new poke list component.

--- src/App.js
+++ src/App.js
@@ -2,34 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'whatwg-fetch';
+import PokeList from './components/PokeList';
+import { Col } from 'react-bootstrap/lib/';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
       pokemon: []
     };
+
     this.loadPokemon = this.loadPokemon.bind(this);
   }
 
   loadPokemon(url) {
     fetch(url)
-    .then(response => {
-      return response.json();
-    }).then(json => {
-      console.log(json)
-      this.setState({
-        pokemon: json.results
-      });
-      console.log(this.state);
-    }).catch(err => {
-      console.log(err)
-    })
+      .then(response => {
+        return response.json();
+      }).then(json => {
+        console.log(json);
+        let pages = Math.round(json.count / this.state.limit);
+
+        this.setState({
+          pokemon: json.results,
+          totalPages: pages,
+          count: json.count
+        });
+        console.log(this.state)
+      }).catch(err => {
+        console.log(err)
+      })
   }
 
-// call method before page loads
   componentWillMount() {
-    this.loadPokemon(`${this.props.baseUrl}/pokemon`);
+    this.loadPokemon(`${this.props.baseUrl}/pokemon/`);
   }
 
   render() {
@@ -39,9 +47,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Col sm={8} md={10} smOffset={2} mdOffset={1} >
+          <PokeList listOfPokemon={this.state.pokemon} />
+        </Col>
+
       </div>
     );
   }
